8e092262c9bd905dfc82a35c9927d7e1783f0f97
Vladimir Skok <skoksworks@gmail.com>
2015-06-06 03:02:53 -0700

    Add gulp clean, sassdoc, gh-pages tasks and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
-var gulp 			= require('gulp');
-var	sass 			= require('gulp-ruby-sass');
+var gulp = require('gulp');
+var	sass = require('gulp-ruby-sass');
+var sassdoc = require('sassdoc');
+var deploy = require('gulp-gh-pages');
+var clean = require('gulp-clean');
 
+var SASSDOC_OPTIONS = {
+	dest: './sassdoc/',
+	verbose: true,
+    basePath: 'https://github.com/SassySuit/sassy-validation',
+    googleAnalytics: 'UA-63553761-1',
+};
 
-gulp.task('default', function() {
+gulp.task('clean', function () {
+    return gulp.src('./sassdoc/', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('sassdoc', ['clean'], function() {
+	return gulp.src('./stylesheets/**/*.scss')
+		.pipe(sassdoc(SASSDOC_OPTIONS));
+});
+
+gulp.task('default', ['sassdoc'], function() {
 	return sass('./stylesheets/_sassy-exists.scss', {
     		style: 'expanded',
     		precision: 10,
@@ -14,3 +33,13 @@ gulp.task('default', function() {
 gulp.task('watch', function() {
 	gulp.watch('**/*.scss', ['default']);
 });
+
+gulp.task('deploy', function() {
+  return gulp.src('./sassdoc/**/*')
+    .pipe(deploy());
+});
+
+var gulp = require('gulp');
+var plugins = {
+  clean: require('gulp-clean'),
+};
